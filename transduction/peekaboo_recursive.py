bde4f6167b96d552af677a32c551186c460ae346
Tim Vieira <tim.f.vieira@gmail.com>
2025-11-29 20:58:03 -0500

    added recursive, batched computation of next-target-symbol optimal DFA-decomposition.

--- transduction/peekaboo_recursive.py
+++ transduction/peekaboo_recursive.py
@@ -10,78 +10,53 @@ from arsenal import colors
 from collections import deque
 
 
-# [2025-11-21 Fri] This version of the next symbold prediction algorithm
-# enumerates strings (and states).  We also have a version that enumrates just
-# the states.
-class PeekabooStrings:
+class Peekaboo:
+    """
+    Recursive, batched computation of next-target-symbol optimal DFA-decomposition.
+    """
 
     def __init__(self, fst, max_steps=float('inf')):
         self.fst = fst
         self.source_alphabet = fst.A - {EPSILON}
         self.target_alphabet = fst.B - {EPSILON}
         self.max_steps = max_steps
+        self._cache = {}
 
     def __call__(self, target):
+        val = self._cache.get(target)
+        if val is None:
+            val = self._cache[target] = self.compute(target)
+        return val
+
+    def compute(self, target):
+
+        dfa = BufferedRelevance(self.fst, target).det()
+
+        if len(target) == 0:
+            worklist = deque()
+            states = set()
+            arcs = []
+
+            for state in dfa.start():
+                worklist.append(state)
+                states.add(state)
+
+        else:
+
+            parent = self(target[:-1])[target[-1]]
+
+            # put previous and Q and R states on the worklist
+            worklist = deque()
+            worklist.extend(parent.quotient.stop)
+            worklist.extend(parent.remainder.stop)
+
+            # TODO: copying is slow
+            states = parent.quotient.states.copy()
+            arcs = [(i,x,j) for i in states for x, j in parent.quotient.arcs(i)]
+
+
         precover = {y: PrecoverDecomp(set(), set()) for y in self.target_alphabet}
-        worklist = deque()
 
-        dfa = PeekabooPrecover(self.fst, target).det()
-        for state in dfa.start():
-            worklist.append(('', state))
-
-        t = 0
-        N = len(target)
-        while worklist:
-            (xs, state) = worklist.popleft()
-            t += 1
-            if t > self.max_steps:
-                print(colors.light.red % 'stopped early')
-                break
-
-            relevant_symbols = {ys[N] for _, ys in state if len(ys) > N}
-
-            # Shortcut: At most one of the `relevant_symbols` can be
-            # continuous. If we find one, we can stop expanding.
-            continuous = set()
-            for y in relevant_symbols:
-                dfa_filtered = FilteredDFA(dfa=dfa, fst=self.fst, target=target + y)
-                #assert dfa_filtered.materialize().equal(Precover(self.fst, target + y).min)
-                #print('ok:', repr(target + y))
-                if dfa_filtered.accepts_universal(state, self.source_alphabet):
-                    precover[y].quotient.add(xs)
-                    continuous.add(y)
-                elif dfa_filtered.is_final(state):
-                    precover[y].remainder.add(xs)
-            assert len(continuous) <= 1
-            if continuous:
-                continue    # we have found a quotient and can skip
-
-            for x, next_state in dfa.arcs(state):
-                worklist.append((xs + x, next_state))
-
-        return precover
-
-
-#class PeekabooStates:
-class Peekaboo:
-
-    def __init__(self, fst, max_steps=float('inf')):
-        self.fst = fst
-        self.source_alphabet = fst.A - {EPSILON}
-        self.target_alphabet = fst.B - {EPSILON}
-        self.max_steps = max_steps
-
-    def __call__(self, target, return_strings=True):
-        precover = {y: PrecoverDecomp(set(), set()) for y in self.target_alphabet}
-        worklist = deque()
-        states = set()
-
-        dfa = PeekabooPrecover(self.fst, target).det()
-        for state in dfa.start():
-            worklist.append(state)
-            states.add(state)
-
-        arcs = []
 
         t = 0
         N = len(target)
@@ -98,13 +73,13 @@ class Peekaboo:
             # continuous. If we find one, we can stop expanding.
             continuous = set()
             for y in relevant_symbols:
-                dfa_filtered = FilteredDFA(dfa=dfa, fst=self.fst, target=target + y)
+                dfa_truncated = TruncatedDFA(dfa=dfa, fst=self.fst, target=target + y)
                 #assert dfa_filtered.materialize().equal(Precover(self.fst, target + y).min)
                 #print('ok:', repr(target + y))
-                if dfa_filtered.accepts_universal(state, self.source_alphabet):
+                if dfa_truncated.accepts_universal(state, self.source_alphabet):
                     precover[y].quotient.add(state)
                     continuous.add(y)
-                elif dfa_filtered.is_final(state):
+                elif dfa_truncated.is_final(state):
                     precover[y].remainder.add(state)
             assert len(continuous) <= 1
             if continuous:
@@ -114,7 +89,6 @@ class Peekaboo:
                 if next_state not in states:
                     worklist.append(next_state)
                     states.add(next_state)
-
                 arcs.append((state, x, next_state))
 
         foo = {y: PrecoverDecomp(set(), set()) for y in self.target_alphabet}
@@ -122,19 +96,13 @@ class Peekaboo:
             Q, R = precover[y]
             q = FSA(start=set(dfa.start()), arcs=arcs, stop=Q)
             r = FSA(start=set(dfa.start()), arcs=arcs, stop=R)
-            if return_strings:
-                foo[y] = PrecoverDecomp(
-                    set(q.min().language(float('inf'))),
-                    set(r.min().language(float('inf'))),
-                )
-            else:
-                foo[y] = PrecoverDecomp(q.trim(), r.trim())
+            foo[y] = PrecoverDecomp(q.trim(), r.trim())
 
         return foo
 
 
 # TODO: in order to predict EOS, we need to extract the preimage from Q and R
-class PeekabooPrecover(Lazy):
+class BufferedRelevance(Lazy):
     """NOTE: this is a semi-automaton as it does not have an `is_final` method.
 
     It implements a state space that tracks the states of an FST `fst` along
@@ -144,6 +112,9 @@ class PeekabooPrecover(Lazy):
 
     """
 
+    # TODO: this machine is not longer specific to the peekaboo construction it
+    # is just the target-side buffering construction with the target-relevance
+    # filter.
     def __init__(self, fst, target):
         self.fst = fst
         self.target = target
@@ -152,24 +123,12 @@ class PeekabooPrecover(Lazy):
         (i, ys) = state
         n = len(ys)
         N = len(self.target)
-        if ys == self.target and n >= N:
+        m = min(n, N)
+        if ys[:m] == self.target[:m]:
             for x,y,j in self.fst.arcs(i):
                 if y == EPSILON:
                     yield (x, (j, ys))
                 else:
-                    yield (x, (j, ys + y))
-
-        # Note: we truncate the buffer after the (N+1)th symbol
-        # XXX: In the recursive algorithm, we would not do this!
-        elif ys.startswith(self.target) and n == N + 1:
-            for a,b,j in self.fst.arcs(i):
-                yield (a, (j, ys))
-
-        elif self.target.startswith(ys) and n < N:
-            for x,y,j in self.fst.arcs(i):
-                if y == EPSILON:
-                    yield (x, (j, ys))
-                elif y == self.target[len(ys)]:
                     yield (x, (j, ys + y))
 
     def start(self):
@@ -177,7 +136,7 @@ class PeekabooPrecover(Lazy):
             yield (i, '')
 
 
-class FilteredDFA(Lazy):
+class TruncatedDFA(Lazy):
     """NOTE: This class augments a determinized `PeekabooPrecover` semi-automaton by
     adding an appropriate `is_final` method so that it is a valid finite-state
     automaton that encodes `Precover(fst, target)`.
@@ -191,8 +150,19 @@ class FilteredDFA(Lazy):
     def start(self):
         return self.dfa.start()
 
+    def refine(self, frontier):
+        # clip the target side side to `y` in order to mimick the states of the
+        # composition machine that we used in the new lazy, nonrecursive
+        # algorithm.
+        N = len(self.target)
+        return frozenset({
+            (i, ys[:N]) for i, ys in frontier
+            if ys[:min(N, len(ys))] == self.target[:min(N, len(ys))]
+        })
+
     def arcs(self, state):
-        return self.dfa.arcs(state)
+        for x, next_state in self.dfa.arcs(state):
+            yield x, self.refine(next_state)
 
     def is_final(self, state):
         return any(ys.startswith(self.target) and self.fst.is_final(i) for (i, ys) in state)
@@ -222,11 +192,19 @@ class recursive_testing:
         if depth == 0: return
         want = {y: self.reference(target + y) for y in self.reference.target_alphabet}
         have = self.peekaboo(target)
-        assert have == want, f"""\ntarget = {target!r}\nhave = {have}\nwant = {want}\n"""
+        #assert have == want, f"""\ntarget = {target!r}\nhave = {have}\nwant = {want}\n"""
+        assert_equal_decomp_map(have, want)
+
         for y in want:
             if self.verbosity > 0: print('>', repr(target + y))
             if want[y].quotient or want[y].remainder:   # nonempty
                 self.run(target + y, depth - 1)
+
+
+def assert_equal_decomp_map(have, want):
+    for y in have | want:
+        assert have[y].quotient.equal(want[y].quotient)
+        assert have[y].remainder.equal(want[y].remainder)
 
 
 def test_abc():
@@ -237,13 +215,13 @@ def test_abc():
     have = p(target)
     tmp = EagerNonrecursive(fst)
     want = {y: tmp(target + y) for y in tmp.target_alphabet}
-    assert have == want
+    assert_equal_decomp_map(have, want)
 
     target = 'abc'
     have = p(target)
     tmp = EagerNonrecursive(fst)
     want = {y: tmp(target + y) for y in tmp.target_alphabet}
-    assert have == want
+    assert_equal_decomp_map(have, want)
 
     recursive_testing(fst, '', depth=5)
 
@@ -257,10 +235,10 @@ def test_samuel():
     tmp = EagerNonrecursive(fst)
     want = {y: tmp(target + y) for y in tmp.target_alphabet}
 
-    print(have)
-    print(want)
+    #print(have)
+    #print(want)
 
-    assert have == want
+    assert_equal_decomp_map(have, want)
 
     recursive_testing(fst, '', depth=5)
 
@@ -316,19 +294,21 @@ def test_newspeak2():
     tmp = EagerNonrecursive(fst)
     want = {y: tmp(target + y) for y in tmp.target_alphabet}
 
+    assert_equal_decomp_map(have, want)
+
     #print('have=', have)
     #print('want=', want)
 
-    for y in have | want:
-        if have.get(y) == want.get(y):
-            print(colors.mark(True), repr(y))
-        else:
-            print(colors.mark(False), repr(y))
-            print('  have=', have.get(y))
-            print('  want=', want.get(y))
-            #Precover(fst, target + y).check_decomposition(*want[y], throw=True)
-            Precover(fst, target + y).check_decomposition(*have[y], throw=False)
-    assert have == want
+#    for y in have | want:
+#        if have.get(y) == want.get(y):
+#            print(colors.mark(True), repr(y))
+#        else:
+#            print(colors.mark(False), repr(y))
+#            print('  have=', have.get(y))
+#            print('  want=', want.get(y))
+#            #Precover(fst, target + y).check_decomposition(*want[y], throw=True)
+#            Precover(fst, target + y).check_decomposition(*have[y], throw=False)
+#    assert have == want
 
     #recursive_testing(fst, '', depth=5)
 
