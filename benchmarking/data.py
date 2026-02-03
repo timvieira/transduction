5125191c26dbd646e39c9c6348934866bdd7636c
Tim Vieira <tim.f.vieira@gmail.com>
2026-02-03 14:52:13 -0500

    ()

---
+++ benchmarking/data.py
@@ -0,0 +1,184 @@
+import re
+
+from datasets import load_dataset
+
+
+def wikitext_detokenize(string: str) -> str:
+    """
+    Wikitext is whitespace tokenized and we remove these whitespaces.
+
+    Taken from https://github.com/NVIDIA/Megatron-LM/blob/main/tasks/zeroshot_gpt2/detokenizer.py
+    """
+    # contractions
+    string = string.replace("s '", "s'")
+    string = re.sub(r"/' [0-9]/", r"/'[0-9]/", string)
+    # number separators
+    string = string.replace(" @-@ ", "-")
+    string = string.replace(" @,@ ", ",")
+    string = string.replace(" @.@ ", ".")
+    # punctuation
+    string = string.replace(" : ", ": ")
+    string = string.replace(" ; ", "; ")
+    string = string.replace(" . ", ". ")
+    string = string.replace("  . ", ". ")
+
+    string = string.replace(" ! ", "! ")
+    string = string.replace(" ? ", "? ")
+    string = string.replace(" , ", ", ")
+    # double brackets
+    string = re.sub(r"\(\s*([^\)]*?)\s*\)", r"(\1)", string)
+    string = re.sub(r"\[\s*([^\]]*?)\s*\]", r"[\1]", string)
+    string = re.sub(r"{\s*([^}]*?)\s*}", r"{\1}", string)
+    string = re.sub(r"\"\s*([^\"]*?)\s*\"", r'"\1"', string)
+    string = re.sub(r"'\s*([^']*?)\s*'", r"'\1'", string)
+    # miscellaneous
+    string = string.replace("= = = =", "====")
+    string = string.replace("= = =", "===")
+    string = string.replace("= =", "==")
+    string = string.replace(" " + chr(176) + " ", chr(176))
+    string = string.replace(" \n", "\n")
+    string = string.replace("\n ", "\n")
+    string = string.replace(" N ", " 1 ")
+    string = string.replace(" 's", "'s")
+
+    return string
+
+
+def load_fasta(T, file_path):
+    sequences = []
+    current_seq = []
+    total_len = 0
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith(">"):
+                if current_seq:
+                    transformed = [T.aa_map[c] for c in list("".join(current_seq))]
+                    sequences.append(transformed)
+                    total_len += len(transformed)
+                    current_seq = []
+            else:
+                current_seq.append(line)
+        if current_seq:
+            transformed = [T.aa_map[c] for c in list("".join(current_seq))]
+            # byte_seq = [str(c) for c in list("".join(current_seq).encode('utf-8'))]
+            sequences.append(transformed)
+            total_len += len(transformed)
+    return sequences, total_len
+
+
+def load_wikitext(split):
+    return load_dataset("wikitext", "wikitext-2-raw-v1", split=split)
+
+
+def load_wikitext_paragraphs(T, split, n=4):
+    """
+    Load the first n paragraphs from the wikitext dataset.
+    """
+    dataset = load_wikitext(split)
+    paragraphs = []
+    for item in dataset:
+        text = item["text"].strip()
+        if text and not text.startswith("="):  # Skip headings
+            detokenized = wikitext_detokenize(text)
+            transduced = T.apply(detokenized)
+            paragraphs.append(transduced)
+            if len(paragraphs) == n:
+                break
+    total_len = 0
+    for i, para in enumerate(paragraphs):
+        total_len += len(para)
+        print(
+            f"Paragraph {i+1} len {len(para)} cumulative length {total_len}:\n{para}\n"
+        )
+    return paragraphs, total_len
+
+
+def load_wikitext_paragraphs_bytes(
+    T,
+    split,
+    tokenizer,
+    n=4,
+    join_paragraphs=False,
+    text_length=None
+):
+    """
+    Load the first n paragraphs from the wikitext dataset.
+    """
+    dataset = load_wikitext(split)
+    paragraphs = []
+    original = []
+    lens = 0
+    for item in dataset:
+        text = item["text"].strip()
+        if text and not text.startswith("="):  # Skip headings
+            detokenized = wikitext_detokenize(text)
+            if text_length:
+                detokenized = detokenized[:text_length]
+            detokenized = tuple(tokenizer.encode(detokenized))
+            fsa = T(detokenized, None)
+            transduced = next(fsa.language(tuple=True))
+
+            if join_paragraphs:
+                paragraphs.extend(transduced)
+            else:
+                paragraphs.append(transduced)
+                original.append(detokenized)
+            lens += 1
+            if lens == n:
+                break
+    total_len = 0
+    for i, para in enumerate(paragraphs):
+        total_len += len(para)
+        print(
+            f"Paragraph {i+1} len {len(para)} cumulative length {total_len}:\n{para}\n"
+        )
+    return paragraphs, original, total_len
+
+
+def load_hf_data_paragraphs_bytes(
+    T,
+    split,
+    n=100,
+    verbose=True,
+    join_paragraphs=False,
+    transducer_name="hf_realpha",
+    dataset_name="JulesBelveze/tldr_news",
+    text_column="content",
+    max_length=None,
+):
+    """
+    Load the first n paragraphs from the wikitext dataset.
+    """
+    dataset_config = None
+    dataset = load_dataset(dataset_name, dataset_config, split="test")
+    dataset = dataset.select(range(n))
+    paragraphs = []
+    original = []
+    lens = 0
+
+    for item in dataset:
+        text = item[text_column].strip()
+        if max_length is not None and len(text) > max_length:
+            text = text[:max_length]
+        if text:
+            fsa = T(text, None)
+            transduced = next(fsa.language())
+
+            if join_paragraphs:
+                paragraphs.extend(transduced)
+            else:
+                paragraphs.append(transduced)
+                original.append(text)
+            lens += 1
+            if lens == n:
+                break
+    total_len = 0
+    for i, para in enumerate(paragraphs):
+        total_len += len(para)
+        print(
+            f"Paragraph {i+1} len {len(para)} cumulative length {total_len}:\n{para}\n"
+        )
+    return paragraphs, original, total_len
---
+++ benchmarking/fsts/realpha.py
@@ -0,0 +1,50 @@
+from genlm.backend.tokenization.bytes import get_byte_vocab
+
+from transduction.fst import FST
+from transduction.fsa import EPSILON
+from transformers import AutoTokenizer
+
+
+def build_realpha(tokenizer_name):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    byte_vocab = get_byte_vocab(tokenizer)
+    tokens = sorted(tokenizer.vocab.items(), key=lambda x: x[1])
+
+    start_state = 0
+
+    fst = FST(start=(start_state,), stop=(start_state,))
+
+    for bytes_vals, (token_str, token) in zip(byte_vocab, tokens):
+        if token_str in tokenizer.all_special_tokens:
+            continue
+
+        current_state = start_state
+        current_input = token
+        for idx, byte_val in enumerate(bytes_vals):
+            if idx == len(bytes_vals) - 1:
+                next_state = start_state
+            else:
+                next_state = len(fst.states)
+            fst.add_arc(
+                current_state,
+                current_input if idx == 0 else EPSILON,
+                byte_val,
+                next_state
+            )
+            current_state = next_state
+            current_input = EPSILON
+
+    return fst
+
+
+if __name__ == "__main__":
+    tok = AutoTokenizer.from_pretrained("gpt2")
+    print("Built tokenizer for gpt2")
+    fst = build_realpha("gpt2")
+    print("Built FST for realpha, states ", len(fst.states))
+    print("Input vocab", len(fst.A))
+    print("Output vocab", len(tok.vocab) - len(tok.all_special_tokens) - 1)
+    # Subtract 1 for epsilon
+    assert len(fst.A) - 1 == len(tok.vocab) - len(tok.all_special_tokens)
+    assert len(fst.B) == 256
+    print("Vocab looks healthy")
